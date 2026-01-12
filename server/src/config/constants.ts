99355e5901ed9676afd6d5bbaa0f94726116a246
MelanDomi <69863379+MelanDomi@users.noreply.github.com>
2026-01-12 11:35:05 -0600

    Create constants.ts

---
+++ server/src/config/constants.ts
@@ -0,0 +1,10 @@
+export const RULES = {
+  SKIP_THRESHOLD_MS: 15_000,
+  COMPLETE_RATIO: 0.7
+};
+
+// Beta prior smoothing for transition skip-rate
+export const BAYES = {
+  ALPHA: 1,
+  BETA: 3
+};
