833a7512c174da25e0281a25b31dbe0974a76499
MelanDomi <69863379+MelanDomi@users.noreply.github.com>
2026-01-13 11:01:49 -0600

    Update playback.ts

--- server/src/spotify/playback.ts
+++ server/src/spotify/playback.ts
@@ -1,9 +1,8 @@
 import { spotifyFetch } from "./spotifyApi.js";
 
-export async function addToQueue(args: { uri: string; deviceId?: string }) {
-  const params = new URLSearchParams({ uri: args.uri });
-  if (args.deviceId) params.set("device_id", args.deviceId);
-
-  // POST /me/player/queue?uri=...
-  await spotifyFetch(`/me/player/queue?${params.toString()}`, "POST");
+export async function transferPlayback(deviceId: string) {
+  await spotifyFetch("/me/player", "PUT", {
+    device_ids: [deviceId],
+    play: true
+  });
 }
